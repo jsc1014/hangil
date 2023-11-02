d7509e0b885f0b4fcf4f3f2c218c29aacc88cf55
jsc1014 <wotlr5209@gmail.com>
2023-11-02 20:44:35 +0900

    init

---
+++ hangil/src/main/java/com/ssafy/hangil/HangilApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.hangil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangilApplication.class, args);
+	}
+
+}
---
+++ hangil/src/test/java/com/ssafy/hangil/HangilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.hangil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

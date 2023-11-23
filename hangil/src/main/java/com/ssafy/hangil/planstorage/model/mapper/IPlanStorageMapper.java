92db89ac2ad97a368d78ac283a1d8ccb558eb2dc
jsc1014 <wotlr5209@gmail.com>
2023-11-23 09:08:49 +0900

    feat: 계획 수정

--- hangil/src/main/java/com/ssafy/hangil/planstorage/model/mapper/IPlanStorageMapper.java
+++ hangil/src/main/java/com/ssafy/hangil/planstorage/model/mapper/IPlanStorageMapper.java
@@ -1,5 +1,8 @@
 package com.ssafy.hangil.planstorage.model.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ssafy.hangil.planstorage.model.PlanStorageDTO;
@@ -9,14 +12,18 @@ public interface IPlanStorageMapper {
 	
 	PlanStorageDTO detailPlan(String planStorageNo);
 	
-	void registPlan(PlanStorageDTO planStorageDTO);
+	void registPlan(PlanStorageDTO planstorageDTO);
 	
 	void deleltePlan(int planStorageNo);
 	
-	void updatePlan(PlanStorageDTO planStorageDTO);
+	void updatePlan(String planStorageName, int boardNo);
 	
 	int sharePlan(int planStorageNo);
 
-	void setBoardNo(int boardNo, int storageNo);
+	void setBoardNo(int boardNo, int planStorageNo);
+
+	List<PlanStorageDTO> getPlanStorageList();
+
+	List<Integer> getPlanList(int planStorageNo);
 	
 }
